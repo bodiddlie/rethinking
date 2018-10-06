5b2b8322638cb544e0dd3094ea747bde4f547b03
bodiddlie <bodiddlie@gmail.com>
2018-10-06 08:43:52 -0700

    fixed test and added quick about slide

--- components/confident-rtl.test.js
+++ components/confident-rtl.test.js
@@ -14,11 +14,11 @@ test('starts at 0', () => {
 test('decrement does - 1', () => {
   const { getByText, getByTestId } = render(<BadFalseConf2 />);
   fireEvent.click(getByText('Decrement'));
-  expect(getByTestId('display')).toHaveTextContent('-1');
+  expect(getByTestId('display')).toHaveTextContent('1 -1');
 });
 
 test('decrement does + 1', () => {
   const { getByText, getByTestId } = render(<BadFalseConf2 />);
   fireEvent.click(getByText('Increment'));
-  expect(getByTestId('display')).toHaveTextContent('1');
+  expect(getByTestId('display')).toHaveTextContent('1 1');
 });
