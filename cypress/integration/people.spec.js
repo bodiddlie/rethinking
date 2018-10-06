beb74957d280c9f190d1cf5c20fecf3c04a94eb1
bodiddlie <bodiddlie@gmail.com>
2018-10-06 10:02:24 -0700

    updated cypress test to be cleaner

--- cypress/integration/people.spec.js
+++ cypress/integration/people.spec.js
@@ -1,18 +1,17 @@
 describe('Random people stuff', () => {
-  it('can decrement a degree', () => {
+  beforeEach(() => {
     cy.server();
     cy.route('/api/people', 'fixture:people.json');
-    cy.route('PUT', '/api/people', {});
+    cy.route('PUT', '/api/people/**', {});
     cy.visit('/');
+  });
+
+  it('can decrement a degree', () => {
     cy.get(':nth-child(1) > div > .btn-dec').click();
     cy.get(':nth-child(1) > div > .counter-value').should('contain', '-1');
   });
 
   it('can increment a degree', () => {
-    cy.server();
-    cy.route('/api/people', 'fixture:people.json');
-    cy.route('PUT', '/api/people', {});
-    cy.visit('/');
     cy.get(':nth-child(1) > div > .btn-dec').click();
     cy.get(':nth-child(1) > div > .counter-value').should('contain', '1');
   });
